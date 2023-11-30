dee8d422dcf83dfbc8b79556bdc720410ce3a60e
Rick H Nguyen <rick248nguyen@gmail.com>
2023-11-30 15:32:47 -0500

    add start button to move scenes

--- story.js
+++ story.js
@@ -1,11 +1,13 @@
 // initialize vars
 let sceneCount;
+let startBtn;
 
 // load assets
 function preload() {
     fancy = loadFont("font3.otf")
 
     schoolPhoto = loadImage("./assets/georgiatechphoto.jpg");
+    culcStairs = loadImage("./assets/culcStairs.png");
 }
 
 // setup
@@ -13,8 +15,14 @@ function setup() {
 	getAudioContext().suspend();
 	rectMode(CENTER);
 	createCanvas(screen.width, screen.height);
-
     sceneCount = 1;
+
+    startBtn = createButton("Start Button");
+    startBtn.position(canvas.width / 2, canvas.height / 2);
+    startBtn.mousePressed(() => {
+        startBtn.remove();
+        sceneCount = 2;
+    })
 }
 
 // draw scenes
@@ -27,18 +35,20 @@ function draw() {
 }
 
 // functions for scenes below //
-
 function titleScreen() {
     // for title text
     fill(252, 174, 200);
 	stroke(120, 34, 62);
 	strokeWeight(4);
-	textFont('Courier New', 70);
+	textFont('Courier New', 50);
 	background(schoolPhoto);
 	textAlign(CENTER);
 	text("Ramblin' Romance", width / 2, 200);
 	noStroke();
 
+
+    /*
+    // for start "button" design
     fill(252, 174, 200);
 	stroke(255);
 	square(width / 2, height / 2, 100, 5);
@@ -47,12 +57,26 @@ function titleScreen() {
 	fill(0);
 	text("START", width / 2, height / 2);
 	noStroke();
+    */
 }
+
+function information() {
+    background(culcStairs);
+}
+
+// ^ insert all scenes above this comment ^ //
 
 
 function storyTime(sceneCount) {
-    if (sceneCount == 1) {
-        titleScreen();
+    switch(sceneCount) {
+        case 1:
+            titleScreen();
+            break;
+        case 2:
+            information();
+            break;
+        default:
+            break;
     }
 }
 
@@ -61,7 +85,7 @@ function textBox(chara) {
 }
 
 function optButtons() {
-
+    // need to implement buttons onClick
 }
 
 // need buttons for user input //
