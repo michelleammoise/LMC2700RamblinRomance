7d0d92980921cf47c53834b6b2a9925194ef3850
Rick H Nguyen <rick248nguyen@gmail.com>
2023-11-30 14:26:35 -0500

    add index and story.js file

---
+++ story.js
@@ -0,0 +1,33 @@
+// initialize vars
+let sceneCount;
+
+// load assets
+function preload() {
+
+}
+
+// setup
+function setup() {
+
+}
+
+// draw scenes
+function draw() {
+
+}
+
+// functions for scenes below //
+
+function storyTime(sceneCount) {
+
+}
+
+function textBox(chara) {
+
+}
+
+function optButtons() {
+
+}
+
+// need buttons for user input //
